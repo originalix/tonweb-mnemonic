625dc289c440774835640c3098147ce540b9aa48
Leon <lixiao.dev@gmail.com>
2024-09-19 10:01:33 +0800

    feat: Export mnemonicToEntropy

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { isPasswordNeeded } from './functions/is-password-needed';
 export { mnemonicToKeyPair, KeyPair } from './functions/mnemonic-to-key-pair';
 export { mnemonicToSeed } from './functions/mnemonic-to-seed';
 export { validateMnemonic } from './functions/validate-mnemonic';
+export { mnemonicToEntropy } from './functions/common'
 
 export * as wordlists from './bip39-wordlists';
